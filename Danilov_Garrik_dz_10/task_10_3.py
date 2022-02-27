1457051013f0924024e89b0426b45783bb98d7a6
GarrikDanilov <garrik_danilov@mail.ru>
2022-02-27 03:55:20 +0300

    add task_10_3.py

--- Danilov_Garrik_dz_10/task_10_3.py
+++ Danilov_Garrik_dz_10/task_10_3.py
@@ -27,6 +27,21 @@ class Cell:
         else:
             raise TypeError('действие допустимо только для экземпляров того же класса')
 
+    def __mul__(self, other):
+        if isinstance(self, Cell) and isinstance(other, Cell):
+            return Cell(self.cells * other.cells)
+        else:
+            raise TypeError('действие допустимо только для экземпляров того же класса')
+
+    def __truediv__(self, other):
+        if isinstance(self, Cell) and isinstance(other, Cell):
+            return Cell(self.cells // other.cells)
+        else:
+            raise TypeError('действие допустимо только для экземпляров того же класса')
+
+    def __floordiv__(self, other):
+        return self / other
+
 
 if __name__ == '__main__':
     cell_1 = Cell(15)
@@ -41,21 +56,21 @@ if __name__ == '__main__':
     sub_cell = cell_1 - cell_3
     print(sub_cell.make_order(6))
 
-    """mul_cell = cell_2 * cell_3
-    print(mul_cell.cells)  # 30
+    mul_cell = cell_2 * cell_3
+    print(mul_cell.cells)
 
     floordiv_cell = cell_2 // cell_3
-    print(floordiv_cell.cells)  # 3
+    print(floordiv_cell.cells)
 
     truediv_cell = cell_1 / cell_2
-    print(truediv_cell.cells)  # 1
+    print(truediv_cell.cells)
 
     try:
         cell_3 - cell_2
     except ValueError as err:
-        print(err)  # недопустимая операция
+        print(err)
 
     try:
         cell_1 * 123
     except TypeError as err:
-        print(err)  # действие допустимо только для экземпляров того же класса"""
+        print(err)
